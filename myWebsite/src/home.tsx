8173063a74c87baa0eef5c072f368e56523eb871
Oscar Song <oscarsjs@uw.edu>
2024-06-19 19:38:36 -0700

    Initialized vite with react ts and installed sass (fresh start)

---
+++ myWebsite/src/home.tsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+    <body>
+      
+    </body>
+    </html>
+  )
+}
+
+export default App
