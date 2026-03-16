ae203a6edc9bde6a0f6ab9bcc25fcb408edb1c51
Oscar Shijie Song <oscarshijiesong@gmail.com>
2026-03-15 20:35:43 -0700

    dynamic font size using clamp for the main title

--- MyX/src/routes/+page.svelte
+++ MyX/src/routes/+page.svelte
@@ -14,18 +14,18 @@
   </ul>
 </nav>
 
-<h3 class="self-center text-3xl mt-10 text-[#707070]">Hello! This is,</h3>
-<h1 class="self-center text-9xl leading-[0.7]">Oscar</h1>
-<h2 class="self-center text-5xl">Song</h2>
-
-<h1 class="absolute top-35 left-1/2 -translate-x-1/2 text-[9rem] z-[-1] text-[#E3E5DF] leading-[0.8]">Seattle,<br> WA</h1>
-
-<h3 class="self-left text-2xl mt-25 ml-6">+1 (206) 694-9786</h3>
-<h3 class="self-left text-2xl ml-6">oscarsjs@uw.edu</h3>
-
-<img src="/ascii_me.svg" alt="Profile" class="absolute bottom-0 right-2 w-70 z-[-1]">
-
-<!-- <rvt-icon name="arrow-down" class="fixed bottom-3 self-center z-[-1] animate-bounce [--size:3rem]"></rvt-icon> -->
-<Icon icon="pixelarticons:arrow-big-down-dash-sharp" class="fixed bottom-3 self-center z-[-1] animate-bounce [--size:3rem] w-10 h-10" />
-
-<div class="h-300"></div>
+<section class="h-screen flex flex-col items-center">
+  <h3 class="md:self-start text-[clamp(1rem,6vw,3rem)] mt-10 text-[#707070]">Hello! This is,</h3>
+  <h1 class="md:self-start text-[clamp(5rem,30vw,12rem)] leading-[0.7]">Oscar</h1>
+  <h2 class="md:self-start text-[clamp(3rem,18vw,7rem)] leading-[0.9]">Song</h2>
+  
+  <h1 class="absolute top-35 left-1/2 md:left-1/4 -translate-x-1/2 text-[9rem] z-[-1] text-[#E3E5DF] leading-[0.8]">Seattle,<br> WA</h1>
+  
+  <h3 class="self-start text-2xl mt-25">+1 (206) 694-9786</h3>
+  <h3 class="self-start text-2xl">oscarsjs@uw.edu</h3>
+  
+  <img src="/ascii_me.svg" alt="Profile" class="absolute top-65 right-2 w-70 z-[-1]">
+  
+  <!-- <rvt-icon name="arrow-down" class="fixed bottom-3 self-center z-[-1] animate-bounce [--size:3rem]"></rvt-icon> -->
+  <Icon icon="pixelarticons:arrow-big-down-dash-sharp" class="fixed bottom-3 self-center z-[-1] animate-bounce [--size:3rem] w-10 h-10" />
+</section>
